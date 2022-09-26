0f212720fac4e435fcf5bcc16c56f96751cf947c
wangyan <woderpg@sina.com>
2022-09-26 14:37:27 +0800

    转账暴露nonce

--- src/main/java/com/crichain/sdk/crichain/SafeTransfer.java
+++ src/main/java/com/crichain/sdk/crichain/SafeTransfer.java
@@ -43,6 +43,29 @@ public class SafeTransfer {
     }
 
     /**
+     * 转账
+     *
+     * @param toAddress 对方地址
+     * @param amount    金额
+     * @param exData    拓展参数
+     * @return JSONObject
+     */
+    public static JSONObject safeTransfer(String callerAddress, int nonce, String priKey, String toAddress, String amount, String exData) {
+        AbiContractUtil.encInstance();
+        //金额转换
+        amount = AbiContractUtil.amountTransition(amount);
+        //创建实例并生成转账txData
+        TransferInfo transferInfo = new TransferInfo(toAddress, amount);
+        String transferToTx = HiChain.getTransferToTx(callerAddress, nonce, priKey, exData, transferInfo);
+        log.info("转账txData:" + transferToTx);
+        //发送请求
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("txData", transferToTx);
+        JSONObject result = HttpClientUtil.doPost(Config.getUrl() + "/chain/transferCric.json", params);
+        return result;
+    }
+
+    /**
      * 交易信息
      *
      * @param hash hash值
